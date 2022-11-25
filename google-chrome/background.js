dc64c20b793df6d3cf2694ad5a12201a39e0233d
Angelo Evangelista <angeloevan.ane@gmail.com>
2022-11-25 16:06:53 -0300

    change: removing background service from chrome version

--- google-chrome/background.js
+++
@@ -1,6 +0,0 @@
-// event to run execute.js content when extension's button is clicked
-chrome.action.onClicked.addListener(execute);
-
-async function execute() {
-  console.log("clicled the extension button")
-}
